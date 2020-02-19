26a76ce1f4f5af4bfdc4e45f4f1e4cad59df243c
Andres Campuzano <campuxano88@gmail.com>
2020-02-19 04:47:44 -0500

    getting input from user.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// const $input = document.getElementById("input");
+// const $addCityButton = document.getElementById("button");
+
+// $addCityButton.addEventListener('submit', add);
+// function add() {
+//     return cityTyped = $input.value;
+// }
+
+const cityTyped = prompt('Search city')
 const city = 'medellin';
 const API_KEY = 'c982d29f34b457c7256dfdf78eff3288';
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityTyped}&appid=${API_KEY}`)
 .then(function (response) {
     console.log(response);
     return response.json();
@@ -11,6 +20,8 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY
 
       const $name = document.getElementById("name");
       $name.innerHTML = data.name;
+      const $country = document.getElementById("country");
+      $country.innerHTML = data.sys.country;
       const $temp = document.getElementById("temp");
       $temp.innerHTML = data.main.temp;
       const $description = document.getElementById("description");
